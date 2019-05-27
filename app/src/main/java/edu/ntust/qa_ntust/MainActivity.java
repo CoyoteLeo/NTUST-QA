f4a3a8431566b60245aa5a81c4c7c4736d50c1bb
weeturn <45900398+weeturn@users.noreply.github.com>
2019-05-28 00:40:50 +0800

    add settings to menu bar

--- app/src/main/java/edu/ntust/qa_ntust/MainActivity.java
+++ app/src/main/java/edu/ntust/qa_ntust/MainActivity.java
@@ -13,6 +13,9 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import edu.ntust.qa_ntust.data.QuestionContract;
@@ -193,5 +196,27 @@ public class MainActivity extends AppCompatActivity implements
         mAdapter.swapCursor(null);
     }
 
+    //  Add the menu to the menu bar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        /* Use AppCompatActivity's method getMenuInflater to get a handle on the menu inflater */
+        MenuInflater inflater = getMenuInflater();
+        /* Use the inflater's inflate method to inflate our visualizer_menu layout to this menu */
+        inflater.inflate(R.menu.visualizer_menu, menu);
+        /* Return true so that the visualizer_menu is displayed in the Toolbar */
+        return true;
+    }
+
+    //  When the "Settings" menu item is pressed, open SettingsActivity
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent startSettingsActivity = new Intent(this, SettingsActivity.class);
+            startActivity(startSettingsActivity);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
 
