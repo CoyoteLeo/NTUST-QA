cd30d23844df3ac5f6440540e4c270a94bea7264
WillyYang <willy142857@gmail.com>
2019-05-30 03:18:03 +0800

    add login page

---
+++ app/src/main/java/edu/ntust/qa_ntust/LoginActivity.java
@@ -0,0 +1,67 @@
+package edu.ntust.qa_ntust;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private final int RC_SIGN_IN = 123;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null) {
+            signIn();
+        } else {
+            Intent goToMain = new Intent(LoginActivity.this,MainActivity.class);
+            startActivity(goToMain);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                // Sign in succeeded
+                Intent goToMain = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(goToMain);
+            } else {
+                // Sign in failed
+                Toast.makeText(this, "Sign In Failed", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void signIn(){
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+
+        // Create sign-in intent
+        Intent intent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setTosAndPrivacyPolicyUrls(
+                        "https://www.ntust.edu.tw/home.php",
+                        "https://www.ntust.edu.tw/home.php"
+                )
+                .setTheme(R.style.LoginTheme)
+                .build();
+
+        startActivityForResult(intent, RC_SIGN_IN);
+    }
+}
